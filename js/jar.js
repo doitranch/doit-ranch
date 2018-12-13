65d1b44dcce6cb4f536fce44f3bbf9f1255331bc
Billy Largent <dirbovine@gmail.com>
2018-12-13 15:24:07 -0700

    Using visual studio + updates
    Java script addition

---
+++ js/jar.js
@@ -0,0 +1,7 @@
+var changeColorGreen = function () {
+    document.getElementById('cool').className = 'cool green';
+}
+
+var changeColorRed = function () {
+    document.getElementById('cool').className = 'cool red';
+}
